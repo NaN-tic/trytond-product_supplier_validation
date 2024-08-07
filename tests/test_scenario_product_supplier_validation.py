da183973c355ef2a410abaa1bfabb8e83aca21a3
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2024-08-07 11:18:34 +0200

    Convert ‘test’ files to .py

---
+++ tests/test_scenario_product_supplier_validation.py
@@ -0,0 +1,110 @@
+import unittest
+from decimal import Decimal
+
+from proteus import Model
+from trytond.exceptions import UserError
+from trytond.modules.account.tests.tools import (create_chart,
+                                                 create_fiscalyear, create_tax,
+                                                 get_accounts)
+from trytond.modules.account_invoice.tests.tools import \
+    set_fiscalyear_invoice_sequences
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.tests.test_tryton import drop_db
+from trytond.tests.tools import activate_modules
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        drop_db()
+        super().setUp()
+
+    def tearDown(self):
+        drop_db()
+        super().tearDown()
+
+    def test(self):
+
+        # Activate modules
+        activate_modules('product_supplier_validation')
+
+        # Create company
+        _ = create_company()
+        company = get_company()
+
+        # Create fiscal year
+        fiscalyear = set_fiscalyear_invoice_sequences(
+            create_fiscalyear(company))
+        fiscalyear.click('create_period')
+
+        # Create chart of accounts
+        _ = create_chart(company)
+        accounts = get_accounts(company)
+        revenue = accounts['revenue']
+        expense = accounts['expense']
+
+        # Create tax
+        tax = create_tax(Decimal('.10'))
+        tax.save()
+
+        # Create parties
+        Party = Model.get('party.party')
+        supplier = Party(name='Supplier')
+        supplier.customer_code = '1234'
+        supplier.save()
+        customer = Party(name='Customer')
+        customer.save()
+
+        # Create account categories
+        ProductCategory = Model.get('product.category')
+        account_category = ProductCategory(name="Account Category")
+        account_category.accounting = True
+        account_category.account_expense = expense
+        account_category.account_revenue = revenue
+        account_category.save()
+        account_category_tax, = account_category.duplicate()
+        account_category_tax.supplier_taxes.append(tax)
+        account_category_tax.save()
+
+        # Create product
+        ProductUom = Model.get('product.uom')
+        unit, = ProductUom.find([('name', '=', 'Unit')])
+        ProductTemplate = Model.get('product.template')
+        template = ProductTemplate()
+        template.name = 'product'
+        template.default_uom = unit
+        template.type = 'goods'
+        template.purchasable = True
+        template.requires_validation_for_purchase = True
+        template.list_price = Decimal('10')
+        template.cost_price_method = 'fixed'
+        template.account_category = account_category_tax
+        product_supplier = template.product_suppliers.new()
+        product_supplier.party = supplier
+        product, = template.products
+        product.cost_price = Decimal('5')
+        template.save()
+        product, = template.products
+
+        # Supplier validation
+        Purchase = Model.get('purchase.purchase')
+        PurchaseLine = Model.get('purchase.line')
+        purchase = Purchase()
+        purchase.party = supplier
+        purchase.invoice_method = 'order'
+        purchase_line = PurchaseLine()
+        purchase.lines.append(purchase_line)
+        purchase_line.product = product
+        purchase_line.quantity = 2.0
+        purchase_line.unit_price = Decimal('10')
+        purchase.save()
+
+        with self.assertRaises(UserError):
+            purchase.click('quote')
+
+        product_supplier, = product.template.product_suppliers
+        product_supplier.validated = True
+        product_supplier.save()
+        purchase.click('quote')
+        self.assertEqual(purchase.state, 'quotation')
+        self.assertEqual(purchase.state, 'quotation')
